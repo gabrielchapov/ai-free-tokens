5d981d2bdbf2cafba2d5e0924753d12a0c459007
Gabriel <uliseschapov97@gmail.com>
2026-01-02 19:56:13 -0300

    feat: Implemented basic API with 2 services

--- index.ts
+++ index.ts
@@ -1,8 +1,41 @@
+import { groqService } from "./services/groq"
+import { cerebrasService } from "./services/cerebras"
+import type { APIService, ChatMessage } from "./types"
+
+const services: APIService[] = [
+  groqService,
+  cerebrasService,
+]
+
+let currentServiceIndex = 0
+
+function getNextService() {
+  const service = services[currentServiceIndex]
+  currentServiceIndex = (currentServiceIndex + 1) % services.length
+  return service
+}
+
 const server = Bun.serve(
   {
     port: process.env.PORT ?? 3000,
     async fetch(req) {
-      return new Response("API de Bun funcionando correctamente");
+      const { pathname } = new URL(req.url)
+      if (req.method === 'POST' && pathname === '/chat') {
+        const { messages } = await req.json() as { messages: ChatMessage[] }
+        const service = getNextService()
+        console.log(`Using service: ${service?.name}`)
+        const stream = await service?.chat(messages)
+
+        return new Response(stream, {
+          headers: {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+          }
+        })
+      }
+
+      return new Response("Not found", { status: 404 })
     },
   }
 );
